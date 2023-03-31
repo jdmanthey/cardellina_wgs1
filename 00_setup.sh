8fb56379e903a21a9c0c3c0c8e082fb6ebe61599
Joe Manthey <jdmanthey@gmail.com>
2023-03-31 14:56:58 -0500

    Update 00_setup.sh

--- 00_setup.sh
+++ 00_setup.sh
@@ -16,3 +16,12 @@ mkdir 07_window_stats_phylo
 mkdir 08_sfs
 mkdir 10_filter
 mkdir 12_filter_vcf
+
+# index reference
+interactive -p quanah
+
+module load intel java bwa
+
+samtools faidx GCA_001746935.2_mywa_2.1_genomic.fna
+bwa index GCA_001746935.2_mywa_2.1_genomic.fna
+java -jar picard.jar CreateSequenceDictionary R=/home/jmanthey/references/GCA_001746935.2_mywa_2.1_genomic.fna O=/home/jmanthey/references/GCA_001746935.2_mywa_2.1_genomic.dict
